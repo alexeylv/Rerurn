87f143a1af6d19cbd4bd61a3fdaaa8fe5e829919
alexeylv <101318447+alexeylv@users.noreply.github.com>
2024-01-22 14:44:20 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int lenght; //здесь обЪявляется переменная
+  lenght = 7; //здесь присваивается значение переменной
+  cout << "Длина равна ";
+  cout << lenght; //здесь выводится значение переменной
+  return 0;
+}
